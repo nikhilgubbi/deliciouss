a8ec8e2e8f1b61486967878ecb797331edac4bfc
nikhilgubbi <nikhilgubbi@gmail.com>
2022-04-08 15:53:25 +0530

    React Application Practice

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './Home';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Cuisine from "./Cuisine";
+
+function Pages() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cuisine" element={<Cuisine />} />
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Pages
