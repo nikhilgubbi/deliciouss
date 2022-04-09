265c47b9baa5d09fc2bdb56d6cc3d5b621ee218e
nikhilgubbi <nikhilgubbi@gmail.com>
2022-04-09 14:12:06 +0530

    React App

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import styled from "styled-components";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
+import { Link } from "react-router-dom";
 
 
 function Veggie() {
@@ -49,9 +50,11 @@ function Veggie() {
                             <SplideSlide key={recipe.id}>
 
                                 <Card>
-                                    <p>{recipe.title}</p>
-                                    <img src={recipe.image} alt={recipe.title} />
-                                    <Gradient />
+                                    <Link to={'/recipe/' + recipe.id}>
+                                        <p>{recipe.title}</p>
+                                        <img src={recipe.image} alt={recipe.title} />
+                                        <Gradient />
+                                    </Link>
                                 </Card>
                             </SplideSlide>
                         );
