faa1277ed7775419eb68ef7d40872b8efaacb668
nikhilgubbi <nikhilgubbi@gmail.com>
2022-04-09 15:39:27 +0530

    React App

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import { BrowserRouter } from 'react-router-dom';
 import Search from './components/Search';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Search/>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
     </div>
   );
 }
+
+
+
 
 export default App;
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 function Searched() {
 
@@ -31,8 +32,10 @@ function Searched() {
             {searchedRecipes.map((item) => {
                 return (
                     <Card key={item.id}>
-                        <img src={item.image} alt="" />
-                        <h4>{item.title}</h4>
+                        <Link to={'/recipe/' + item.id}>
+                            <img src={item.image} alt="" />
+                            <h4>{item.title}</h4>
+                        </Link>
                     </Card>
                 )
             })}
