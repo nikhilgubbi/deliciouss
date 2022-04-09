4b023502b42f99f2cebc7f165109b7d74acf503f
nikhilgubbi <nikhilgubbi@gmail.com>
2022-04-09 15:28:23 +0530

    React App

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -10,6 +10,8 @@ function Recipe() {
 
     const [details, setDetails] = useState({});
 
+    const [activeTab, setActiveTab] = useState('instructions');
+
     const fetchDetails = async () => {
         const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY
             }`);
@@ -29,9 +31,24 @@ function Recipe() {
             <h2>{details.title}</h2>
             <img src={details.image} alt="" />
         </div>
+
             <Info>
-                <Button>Instructions</Button>
-                <Button>Ingredients</Button>
+                <Button className={activeTab === 'instructions' ? 'active' : ''} onClick={() => setActiveTab('instructions')}>Instructions</Button>
+
+                <Button className={activeTab === 'ingredients' ? 'active' : ''} onClick={() => setActiveTab('ingredients')}>Ingredients</Button>
+                {activeTab === 'instructions' && (
+                    <div>
+                        <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+                        <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+                    </div>)}
+                {activeTab === 'ingredients' && (<ul>
+                    {details.extendedIngredients.map((ingredient) =>
+                        <li key={ingredient.id}>{ingredient.original}</li>
+                    )}
+                </ul>
+
+                )};
+
             </Info>
         </DetailWrapper>
 
